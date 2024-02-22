225e85dd396beeb938d1b50f00426fb0398ce0f7
syedjazib456 <syedjazib456@gmail.com>
2024-02-21 18:25:04 -0800

    WebAPI

---
+++ FullStackAPI/FullStackAPI/FullStackAPI/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using FullStackAPI.Data;
+using FullStackAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public EmployeesController(ApplicationDbContext dbContext) { 
+        this._dbcontext=dbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployee()
+        {
+            var employees = await _dbcontext.Employees.ToListAsync();
+            return Ok(employees);
+            }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployees(Employee employee)
+        {
+            await _dbcontext.Employees.AddAsync(employee);
+            await _dbcontext.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
---
+++ FullStackAPI/FullStackAPI/FullStackAPI/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public double Salary { get; set; }
+        public int Age {  get; set; }
+    }
+}
